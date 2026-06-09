f6995c55be872d61df8e0d2ea9a951cc139b7ec5
Aleksandr Boiarnikov <111020155+gmvrpw@users.noreply.github.com>
2026-06-09 18:31:07 +0300

    Add webidl attributes typing

--- lib/webidl/02-idl/05-idl-members/02-idl-attributes/dfn-attribute.ts
+++ lib/webidl/02-idl/05-idl-members/02-idl-attributes/dfn-attribute.ts
@@ -3,7 +3,10 @@ import type { Member, Type } from "@webidl";
 /** @see https://webidl.spec.whatwg.org/#dfn-attribute */
 export interface Attribute<T> {
   memberType: "attribute";
+  keywords: Set<string>;
   type: Type<T>;
+  getterSteps(): T;
+  setterSteps(value: T): void;
 }
 
 export function isAttribute<T = unknown>(
