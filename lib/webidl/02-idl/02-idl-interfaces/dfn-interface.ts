425d89f9351d2e432c80244ffe2a23560264f3c6
gmvrpw <dev@gmvr.pw>
2026-06-10 23:15:38 +0300

    Add static members support for interfaces

--- lib/webidl/02-idl/02-idl-interfaces/dfn-interface.ts
+++ lib/webidl/02-idl/02-idl-interfaces/dfn-interface.ts
@@ -1,6 +1,9 @@
 import type { Member, Identifier } from "@webidl";
 
+const staticMembers: unique symbol = Symbol.for("@t15i/webidl/staticMembers");
+
 /** @see https://webidl.spec.whatwg.org/#dfn-interface */
 export interface Interface {
+  [staticMembers]: Record<Identifier, Member>;
   [key: Identifier]: Member;
 }
