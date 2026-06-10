cb65cf8a95361adb41de57416edec059c67f76f2
Aleksandr Boiarnikov <111020155+gmvrpw@users.noreply.github.com>
2026-06-10 23:22:23 +0300

    Add interface static members support
    * Add static members support for interfaces
    
    * Export symbol for accessing static members namespace

--- lib/webidl/02-idl/02-idl-interfaces/dfn-interface.ts
+++ lib/webidl/02-idl/02-idl-interfaces/dfn-interface.ts
@@ -1,6 +1,11 @@
 import type { Member, Identifier } from "@webidl";
 
+export const staticMembers: unique symbol = Symbol.for(
+  "@t15i/webidl/staticMembers",
+);
+
 /** @see https://webidl.spec.whatwg.org/#dfn-interface */
 export interface Interface {
+  [staticMembers]: Record<Identifier, Member>;
   [key: Identifier]: Member;
 }
