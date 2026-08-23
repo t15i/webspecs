b65ec5c315739e89ec8c118db5b7b3950f88065c
Aleksandr Boiarnikov <dev@gmvr.pw>
2026-08-23 13:05:06 +0300

    Fix issues from the review

--- lib/webidl/02-idl/13-idl-types/33-idl-annotated-types/extended-attributes-applicable-to-types.ts
+++ lib/webidl/02-idl/13-idl-types/33-idl-annotated-types/extended-attributes-applicable-to-types.ts
@@ -1,4 +1,6 @@
+/** @see https://webidl.spec.whatwg.org/#extended-attributes-applicable-to-types */
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface TypeExtendedAttributes {}
 
+/** @see https://webidl.spec.whatwg.org/#extended-attributes-applicable-to-types */
 export type ApplicableToTypeExtendedAttribute = keyof TypeExtendedAttributes;
